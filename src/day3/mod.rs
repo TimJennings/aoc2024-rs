8b94ae33d0150d43559de0313ccd8153ec4849ca
Tim Jennings <10922362+TimJennings@users.noreply.github.com>
2024-12-03 22:08:23 +0000

    refactored day3 to use fold0 to process the tokens as they're emited by the parser rather than assembling them all into a vec

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -2,38 +2,26 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take},
     combinator::map,
-    multi::many0,
+    multi::{fold_many0, many0},
     sequence::tuple,
     IResult,
 };
 
 use crate::common::{self, nom_utils::parser_u64};
 
-pub fn run() {
-    let input = common::string_utils::read_file_to_string("input/day3.txt");
-    let muls = many0(parse_mul)(&input).unwrap();
+#[derive(Debug)]
+struct State {
+    count: u64,
+    enabled: bool,
+}
 
-    let mut sum = 0;
-    let mut enabled = true;
-    for mul in muls.1 {
-        match mul {
-            Token::Char(_) => continue,
-            Token::Do(_) => {
-                enabled = true;
-                continue;
-            }
-            Token::Dont(_) => {
-                enabled = false;
-                continue;
-            }
-            Token::Mul(left, right) => {
-                if enabled {
-                    sum = sum + (left * right);
-                }
-            }
+impl State {
+    pub fn new() -> Self {
+        State {
+            count: 0,
+            enabled: true,
         }
     }
-    println!("count is :{}", sum);
 }
 
 #[derive(Debug)]
@@ -43,6 +31,7 @@ enum Token<'a> {
     Do(&'a str),
     Dont(&'a str),
 }
+
 fn parse_mul(input: &str) -> IResult<&str, Token> {
     alt((
         map(tag("do()"), Token::Do),
@@ -55,11 +44,36 @@ fn parse_mul(input: &str) -> IResult<&str, Token> {
     ))(input)
 }
 
+pub fn run() {
+    let input = common::string_utils::read_file_to_string("input/day3.txt");
+    let state = fold_many0(parse_mul, State::new, |mut acc: State, token: Token| {
+        match token {
+            Token::Char(_) => {}
+            Token::Do(_) => {
+                acc.enabled = true;
+            }
+            Token::Dont(_) => {
+                acc.enabled = false;
+            }
+            Token::Mul(left, right) => {
+                if acc.enabled {
+                    acc.count = acc.count + (left * right);
+                }
+            }
+        }
+        acc
+    })(&input)
+    .unwrap();
+    println!("count is {}", state.1.count);
+}
+
 #[cfg(test)]
 mod test {
-    use nom::multi::many0;
+    use nom::multi::{fold_many0, many0};
 
-    use super::parse_mul;
+    use crate::common;
+
+    use super::{parse_mul, State, Token};
 
     #[test]
     pub fn test() {
@@ -73,5 +87,30 @@ mod test {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         let mul = many0(parse_mul)(input).unwrap();
         println!("{:?}", mul);
+    }
+
+    #[test]
+    pub fn fold() {
+        let input = common::string_utils::read_file_to_string("input/day3.txt");
+
+        let state = fold_many0(parse_mul, State::new, |mut acc: State, token: Token| {
+            match token {
+                Token::Char(_) => {}
+                Token::Do(_) => {
+                    acc.enabled = true;
+                }
+                Token::Dont(_) => {
+                    acc.enabled = false;
+                }
+                Token::Mul(left, right) => {
+                    if acc.enabled {
+                        acc.count = acc.count + (left * right);
+                    }
+                }
+            }
+            acc
+        })(&input)
+        .unwrap();
+        println!("{:?}", state);
     }
 }
